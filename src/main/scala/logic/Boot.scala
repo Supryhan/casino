babe37a49bb07333ab02957ad9601a8c0b47f30b
Vitalii Supryhan <suprigan@gmail.com>
2019-10-14 11:05:03 +0300

    init casino

--- src/main/scala/logic/Boot.scala
+++ src/main/scala/logic/Boot.scala
@@ -11,6 +11,7 @@ import akka.stream.ActorMaterializer
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.io.StdIn
+import scala.util.{Success, Try}
 
 object Boot extends App {
 
@@ -21,27 +22,28 @@ object Boot extends App {
 
   var users: Map[UserId, (Int, Int, Double)] = Map.empty
 
-  def process(in: (Int, Int, Double)): (Int, Int, Double) = {
+  def processBet(in: (Int, Int, Double)): (Int, Int, Double) = {
     val (balance, bet, probability) = in
     val r = new scala.util.Random(100)
     val win = if (r.nextFloat * 100 < probability) bet else 0
     (balance - bet + win, bet + 5, probability - probability / 100)
   }
+  def getUuid(value: String): Try[Boolean] = Try(UUID.fromString(value)).flatMap(uuid => Try(users.contains(uuid)))
 
   val route: Route =
     path("push") {
       optionalCookie("userName") {
-        case Some(v) if users.contains(UUID.fromString(v.value)) =>
+        case Some(v) if getUuid(v.value).get =>
           get { ctx =>
             val userId = UUID.fromString(v.value)
-            users += (userId -> process(users(userId)))
+            users += (userId -> processBet(users(userId)))
             ctx.complete(Future(s"Your balance is: ${users(userId)._1}, your bet is: ${users(userId)._2}"))
           }
         case _ =>
           val uuid = UUID.randomUUID()
           setCookie(HttpCookie("userName", value = uuid.toString)) {
             get { ctx =>
-              users += (uuid -> process((100, 10, 2.0)))
+              users += (uuid -> processBet((100, 10, 2.0)))
               ctx.complete(Future(s"Your balance is: ${users(uuid)._1}, your bet is: ${users(uuid)._2}"))
             }
           }
