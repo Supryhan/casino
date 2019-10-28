f3fd54234445daf3025834d4dfe1bb8eac99fa87
vitalii supryhan <suprigan@gmail.com>
2019-10-28 13:07:09 +0200

    java

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
+import fastparse.all
+
 name := "casino"
 
 version := "0.1"
@@ -6,5 +8,11 @@ sbtVersion := "1.3.2"
 
 scalaVersion := "2.13.1"
 
-libraryDependencies += "com.typesafe.akka" %% "akka-http"   % "10.1.10"
-libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.23"
+libraryDependencies ++= Seq(
+    "com.typesafe.akka" %% "akka-http"   % "10.1.10",
+    "com.typesafe.akka" %% "akka-stream" % "2.5.23",
+    "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.23",
+    "com.typesafe.akka" %% "akka-http-testkit" % "10.1.10",
+    "org.scalatest" %% "scalatest-wordspec" % "3.2.0-M1",
+    "org.scalatest" %% "scalatest" % "3.0.8",
+)
--- src/test/scala/WebCookieSpec.scala
+++ src/test/scala/WebCookieSpec.scala
@@ -1,3 +1,18 @@
-class WebCookieSpec {
+import akka.actor.ActorRef
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import logic.Boot
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{Matchers, WordSpec}
 
-}
+class WebCookieSpec extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
+
+ /* "The casino" should {
+    "returns new user" in {
+      Get("/push") ~>  Route.seal(Boot.route) ~> check {
+        status should === StatusCodes.OK
+        responseAs[String] shouldEqual "user added"
+      }
+}}*/}
